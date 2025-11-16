ff6aac9c4ddb826e28336201c7c3f7910af4333b
Daniel <dandcalvo@gmail.com>
2025-11-16 02:32:52 -0800

    fix: remove supabase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from api.SupabaseRouter import router as supabase_router
 from api.AuthenticationRouter import router as authentication_router
 
 app = FastAPI()
 
 app.include_router(authentication_router, prefix="/auth")
-app.include_router(supabase_router, prefix="")
--- services/SupabaseAdapters/__init__.py
+++
@@ -1,8 +0,0 @@
-# SupabaseAdapters package
-# This package contains all Supabase adapter modules for different tables
-
-from . import equipment
-from . import users
-from . import parks
-
-__all__ = ["equipment", "users", "parks"]
